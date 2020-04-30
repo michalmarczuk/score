4a72710cbb26bd38fa52ab2f2840524a235bc7b0
Michal Marczuk <MMarczu@rockwellautomation.com>
2020-04-30 11:31:51 +0200

    Added who is winning feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {playerScore} from 'src/app/score/score.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'score';
+  title: string = 'score';
+  players: Player[] = [
+    {
+      name: 'Kamilka',
+      score: 0,
+      isWinning: false
+    },
+    {
+      name: 'Michał',
+      score: 0,
+      isWinning: false
+    }
+  ]
+
+  updateStatus(playerScore: playerScore) {
+    // Update score on player
+    this.players.find(player => player.name === playerScore.playerName).score = playerScore.score;
+
+    // Set isWinning to false on each player
+    this.players.forEach(player => player.isWinning = false);
+
+    const scores: number[] = this.players.map(player => player.score);
+    // Return if it is a tie
+    if (new Set(scores).size !== scores.length) {
+      return;
+    }
+
+    const maxScore = Math.max(...scores);
+    this.players.find(player => player.score === maxScore).isWinning = true;
+
+  }
+}
+
+interface Player {
+  name: string;
+  score: number;
+  isWinning: boolean;
 }
